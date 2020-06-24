2991740ffbba95e29802c81461bf029f4c18d9e9
nenodias <horacio.dias92@gmail.com>
2020-06-24 12:43:35 -0300

    Initial commit

---
+++ src/test/java/io/github/nenodias/centraldeerros/CentralDeErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nenodias.centraldeerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
